8a931d2374756acfa4d68a37ac8ad93a557ca7fe
Rendy Dwi Proklamanta <rendydwi@myself.com>
2024-06-19 07:10:26 +0000

    update

---
+++ php/conf/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Function to handle graceful shutdown
+graceful_shutdown() {
+  echo "Waiting for 1 minute before shutting down..."
+  sleep 60
+}
+
+# Trap the SIGTERM signal and call the graceful_shutdown function
+trap 'graceful_shutdown' SIGTERM
+
+# Start the application in the background
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf -n &
+
+# Wait for all background processes to finish
+wait
