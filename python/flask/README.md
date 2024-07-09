59facaf667f68b2c3f246ed881c4855566528dda
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-07-09 16:56:32 +0700

    update

---
+++ python/flask/README.md
@@ -0,0 +1,7 @@
+# Run dev mode using Docker
+
+```shell
+docker compose -f docker-compose.development.yml up -d --force-recreate
+```
+
+- Open web : <http://localhost:5000>
---
+++ python/flask/source/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+#Index route
+@app.route("/")
+def hello():
+    env_value = os.environ.get('ENV')
+    return f"<h2 style='color:black'>Ready..!! [{env_value}]</h2>"
+
+if __name__ == "__main__":
+
+   app.run(host='0.0.0.0')
