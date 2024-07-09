86b2497c3a4bdad3d54ce8b9baef054b4e9298c1
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-07-09 17:03:21 +0700

    update

--- python/flask/source/app.py
+++ python/flask/source/app.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
-#Index route
+# Index route
 @app.route("/")
 def hello():
     env_value = os.environ.get('ENV')
-    return f"<h2 style='color:black'>Ready..!! [{env_value}]</h2>"
+    return f"<h2 style='color:black'>Ready [{env_value}]</h2>"
 
 if __name__ == "__main__":
 
