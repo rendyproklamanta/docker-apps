32f13073b37adee4b575a704dadc4e445ee10d24
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-08-02 14:33:41 +0700

    update

--- python/flask/conf/entrypoint.sh
+++ python/flask/conf/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-
 # Function to handle graceful shutdown
 graceful_shutdown() {
-  echo "Waiting for 30 sec before shutting down..."
-  sleep 30
+  echo "Waiting for 1 minute before shutting down..."
+  sleep 60
 }
-
 # Trap the SIGTERM signal and call the graceful_shutdown function
 trap 'graceful_shutdown' SIGTERM
-
 # Start the application in the background
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf -n &
-
+/usr/sbin/ntpd -g && /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf -n &
 # Wait for all background processes to finish
 wait
