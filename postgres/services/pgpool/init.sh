096afd7a2671aa2892ae79ca0568c76bce3c48be
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-08-28 17:14:23 +0700

    move postgres to dedicated repo

--- postgres/services/pgpool/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-
-# Deploy
-echo -e "${RED}**** Deploying service pgpool ****${NC}"
-docker stack deploy --compose-file docker-compose.yml --detach=false postgres
