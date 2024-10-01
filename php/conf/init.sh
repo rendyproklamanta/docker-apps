bbc3b51379ae8f7afc6691a99f0500cd9152fef3
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-10-01 15:38:10 +0700

    use docker secrets for securing credential

--- php/conf/init.sh
+++ php/conf/init.sh
@@ -3,3 +3,4 @@
 echo '*** Creating log directory ***'
 sudo mkdir -p /var/log/nginx && sudo chmod -R 777 /var/log/nginx
 sudo mkdir -p /var/log/php && sudo chmod -R 777 /var/log/php
+sudo mkdir -p /var/log/cron && sudo chmod -R 777 /var/log/cron
---
+++ php/php-ci/README.md
@@ -0,0 +1,4 @@
+# How to development using docker
+
+- Rename .env.development.example to .env.development
+- Change DB_HOST, DB_PORT, etc.. with your own credential!
